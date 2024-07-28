4042a53b640a199b76ac635327a065dab5495a61
Fisher48 <rybakov48@yandex.ru>
2024-07-28 21:01:41 +0300

    added Tests

--- aBSTTest.java
+++ aBSTTest.java
@@ -19,4 +19,26 @@ public class aBSTTest {
         assertEquals(2,aBST.AddKey(100));
     }
 
+    @Test
+    public void addInEmptyArray(){
+        aBST emptyBst = new aBST(3);
+        assertEquals(0,emptyBst.AddKey(50));
+        assertEquals(2,emptyBst.AddKey(75));
+        assertEquals(6,emptyBst.AddKey(85));
+        assertEquals(14,emptyBst.AddKey(90));
+        assertEquals(-1,emptyBst.AddKey(95));
+    }
+
+    @Test
+    public void sizeBSTTest(){
+        aBST bst3depth = new aBST(3);
+        aBST bst4depth = new aBST(4);
+        aBST bst5depth = new aBST(5);
+        aBST bst6depth = new aBST(6);
+        assertEquals(15,bst3depth.Tree.length);
+        assertEquals(31,bst4depth.Tree.length);
+        assertEquals(63,bst5depth.Tree.length);
+        assertEquals(127,bst6depth.Tree.length);
+    }
+
 }
