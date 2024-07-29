0a119ba5d7d88feb83169eaea5e03e2b164f2bb9
Fisher48 <rybakov48@yandex.ru>
2024-07-29 19:09:26 +0300

    changed BS to BST

--- BalancedBST.java
+++ BalancedBST.java
@@ -1,14 +1,14 @@
 import java.util.*;
 
-class BSNode
+class BSTNode
 {
     public int NodeKey; // ключ узла
-    public BSNode Parent; // родитель или null для корня
-    public BSNode LeftChild; // левый потомок
-    public BSNode RightChild; // правый потомок
+    public BSTNode Parent; // родитель или null для корня
+    public BSTNode LeftChild; // левый потомок
+    public BSTNode RightChild; // правый потомок
     public int     Level; // глубина узла
 
-    public BSNode(int key, BSNode parent)
+    public BSTNode(int key, BSTNode parent)
     {
         NodeKey = key;
         Parent = parent;
@@ -19,7 +19,7 @@ class BSNode
 
 class BalancedBST
 {
-    public BSNode Root; // корень дерева
+    public BSTNode Root; // корень дерева
 
     public BalancedBST() {
         Root = null;
@@ -28,36 +28,36 @@ class BalancedBST
     public void GenerateTree(int[] a){
         // создаём дерево с нуля из неотсортированного массива a
         Arrays.sort(a);
-        Root = genBSTArray(a, 0, a.length-1, null, 0);
+        Root = genBSTArray(a, 0, a.length-1, Root, 0);
     }
 
-    public static BSNode genBSTArray(int[] a, int start, int end, BSNode node, int level){
+    public static BSTNode genBSTArray(int[] a, int start, int end, BSTNode root, int level){
         if (start > end) {
             return null;
         }
         int mid = (start + end) / 2;
-        BSNode A = new BSNode(a[mid], node);
-        A.Level = level;
-        A.LeftChild = genBSTArray(a, start, mid - 1, A, level + 1);
-        A.RightChild = genBSTArray(a, mid + 1, end, A, level + 1);
-        return A;
+        BSTNode node = new BSTNode(a[mid], root);
+        node.Level = level;
+        node.LeftChild = genBSTArray(a, start, mid - 1, node, level + 1);
+        node.RightChild = genBSTArray(a, mid + 1, end, node, level + 1);
+        return node;
     }
 
-    public boolean IsBalanced(BSNode root_node) {
+    public boolean IsBalanced(BSTNode root_node) {
         if (root_node == null) {
             return true;
         }
         return Math.abs(getHeight(root_node.LeftChild) - getHeight(root_node.RightChild)) <= 1; // сбалансировано ли дерево с корнем root_node
     }
 
-    public static int getHeight(BSNode node) {
+    public static int getHeight(BSTNode node) {
         if (node == null) {
             return 0;
         }
         return Math.max(getHeight(node.LeftChild),getHeight(node.RightChild)) + 1;
     }
 
-    public void printTree(BSNode node, int depth) {
+    public void printTree(BSTNode node, int depth) {
         if (node == null) {
             return;
         }
